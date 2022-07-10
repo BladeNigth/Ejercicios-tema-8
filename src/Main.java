8a612ab8c8f9f32d5a9710d1516466384343b100
Andres Felipe Brieva Pinedo <53844420+BladeNigth@users.noreply.github.com>
2022-07-09 20:55:40 -0500

    Ejercicio Terminado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        persona mipersona = new persona();
+        mipersona.setEdad(23);
+        mipersona.setNombre("Andres Brieva");
+        mipersona.setTelefono(4364079);
+
+        System.out.println("Nombre: " + mipersona.getNombre() + " Edad: " + mipersona.getEdad() + " Telefono: " + mipersona.getTelefono() );
+
+    }
+
+    static class persona {
+        private int edad;
+        private String nombre;
+        private int telefono;
+
+
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getTelefono() {
+            return telefono;
+        }
+
+        public void setTelefono(int telefono) {
+            this.telefono = telefono;
+        }
     }
 }
